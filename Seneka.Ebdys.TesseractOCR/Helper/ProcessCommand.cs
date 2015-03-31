3c835971a9721b047f9752d192045ca95201d7c8
Sercan Boyraz <sercan.boyraz@hotmail.com>
2015-03-31 22:18:53 +0300

    Ocr start

---
+++ Seneka.Ebdys.TesseractOCR/Helper/ProcessCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seneka.Ebdys.TesseractOCR.Helper
+{
+    public class ProcessCommand
+    {
+        protected static void CommandExecute(string Arguments)
+        {
+            Process p = new Process();
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.FileName = "C:\\Tesseract\\tesseract.exe";
+            info.UseShellExecute = false;
+            info.RedirectStandardError = true;
+            info.RedirectStandardOutput = true;
+            info.WindowStyle = ProcessWindowStyle.Hidden;
+            info.CreateNoWindow = true;
+            //-t .75
+            //info.WorkingDirectory = @"C:\\Tesseract\\";
+            info.Arguments = Arguments;
+            p.StartInfo = info;
+            try
+            {
+                p.Start();
+                p.WaitForExit();
+            }
+            catch (Exception x)
+            {
+                Debug.WriteLine(x.Message);
+            }
+        }
+    }
+}
---
+++ Seneka.Ebdys.TesseractOCR/Program.cs
@@ -0,0 +1,40 @@
+﻿using Seneka.Ebdys.TesseractOCR.Helper;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seneka.Ebdys.TesseractOCR
+{
+    class Program:ProcessCommand
+    {
+        static void Main(string[] args)
+        {
+
+            var command = string.Concat("\"D:/1.tif"+"\""+" "+ "\"D:/2.pdf"+"\"" + " pdf");
+            var ocrResult = OCRExecute(command);
+            if (ocrResult)
+            {
+
+            }
+        }
+
+        public static bool OCRExecute(string command)
+        {
+            bool result = false;
+            try
+            {
+                CommandExecute(command);
+                result = true;
+            }
+            catch (Exception ex)
+            {
+                result = true;
+            }
+            return result;
+        }
+        
+    }
+}
